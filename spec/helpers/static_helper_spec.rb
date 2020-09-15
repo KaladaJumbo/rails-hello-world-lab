8ffd2074aa659d833c9248df52433a05e32a950d
KaladaJumbo <wkalada1@gmail.com>
2020-09-15 05:39:20 -0400

    added static controller and hello_world view with path

---
+++ spec/helpers/static_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the StaticHelper. For example:
+#
+# describe StaticHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+#
+# RSpec.describe StaticHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe "hello_world.html.erb" do 
+
+  it "the file exists?" do 
+
+    file = File.new("#{Rails.root}/app/views/static/hello_world.html.erb")
+
+    expect(File.exist?(file)).to eq(true)
+
+  end
+
+end
